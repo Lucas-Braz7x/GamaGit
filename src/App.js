3a52ffef88f0f5dd2665efc5409b5294f0395648
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-07-15 21:51:18 -0300

    Consumindo a API do github com o axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+function App(props /* propriedades */) {
+  const [usuario, setUsuario] = useState('');
+  
+  function handleSearch(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <p>{usuario}</p>
+      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={event => setUsuario(event.target.value)} />
+      <button type="button" onClick={handleSearch}>Pesquisar</button>
+    </>
   );
 }
 
