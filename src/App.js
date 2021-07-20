fa7fb6945ade0d2c83fa2504878213424e05d3ae
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-07-19 21:05:38 -0300

    Configurando roteamento

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import {} from 'react-router-dom';
+import Routes from './routes';
+
 import './App.css';
 
 function App(props /* propriedades */) {
-  const [usuario, setUsuario] = useState('');
-  
-  function handleSearch(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
-  }
-  return (
 
-    <>
-      <p>{usuario}</p>
-      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={event => setUsuario(event.target.value)} />
-      <button type="button" onClick={handleSearch}>Pesquisar</button>
-    </>
+  return (
+    <Routes />
   );
 }
 
